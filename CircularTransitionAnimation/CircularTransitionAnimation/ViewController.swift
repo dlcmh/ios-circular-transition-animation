02a948e4fe0bca5581f6c8734c1ce056a059ec10
David Chin <dlcmhd@me.com>
2016-10-10 09:59:01 +0800

    stuff

---
+++ CircularTransitionAnimation/CircularTransitionAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CircularTransitionAnimation
+//
+//  Created by David Chin on 10/10/2016.
+//  Copyright © 2016 Dakerr Consulting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # iOS: Custom UIViewController circular transition animation
 
 Based on YouTube video [iOS Swift Tutorial: Create a Circular Transition Animation (Custom UIViewController Transitions)](https://youtu.be/B9sH_VxPPo4)
+
+## Steps
+
+Drag 2 view controllers onto Storyboard
+
